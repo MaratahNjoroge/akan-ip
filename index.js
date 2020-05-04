c0568ce2d16775445a6d1d6ed15c434ca25a4acc
Maratah <maratah7@gmail.com>
2020-05-04 15:46:33 +0300

    added comment user interface.

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 var dateWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+//userInterface
