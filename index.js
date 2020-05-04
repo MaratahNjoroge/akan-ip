0d03c2cc28f5d43541036665a9af077c7b2f97f0
Maratah <maratah7@gmail.com>
2020-05-04 16:02:04 +0300

    added a function radio info.

--- index.js
+++ index.js
@@ -30,3 +30,9 @@ function getInfo(){
     alert("You were born on "+ dateWeek[d] + " and your Akan name is "+ name);
 document.getElementById('akan').innerHTML = "You were born on " + daysOfTheWeek[d] +  " and your Akan name is: " + maleNames[d] ;
 }
+function radioInfo (){
+    var radio = document.getElementsByName('gender');
+    for (var i = 0;  i<radio.length; i++)
+    {
+    if (radio[i].checked==true)
+    {
